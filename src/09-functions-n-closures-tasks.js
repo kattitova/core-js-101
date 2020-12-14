6464494371474b3db6a0dae82c6e66135a0df6a5
Kateryna Tytova <titova.kat.85@gmail.com>
2020-12-14 12:44:45 +0200

    feat: add partialUsingArg solution

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -147,8 +147,9 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+// eslint-disable-next-line no-unused-vars
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn(...args1, ...args);
 }
 
 
